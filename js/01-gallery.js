a94ab78b9c3ddca03a13e533e6e04f7518d5a4b6
YevheniiProshkin <evgeniyprofi@ukr.net>
2022-08-12 18:55:38 +0300

    All done

---
+++ js/01-gallery.js
@@ -0,0 +1,94 @@
+// Задание 1 - галерея изображений
+// Создай галерею с возможностью клика по её элементам и просмотра полноразмерного изображения в модальном окне.
+// Посмотри демо видео работы галереи.
+//     Выполняй это задание в файлах 01-gallery.html и 01-gallery.js. Разбей его на несколько подзадач:
+
+// 1. Создание и рендер разметки по массиву данных galleryItems и предоставленному шаблону элемента галереи.
+// 2. Реализация делегирования на div.gallery и получение url большого изображения.
+// 3. Подключение скрипта и стилей библиотеки модального окна basicLightbox.
+// Используй CDN сервис jsdelivr и добавь в проект ссылки на минифицированные(.min) файлы библиотеки.
+// 4. Открытие модального окна по клику на элементе галереи. Для этого ознакомься с документацией и примерами.
+// 5. Замена значения атрибута src элемента < img > в модальном окне перед открытием.
+// Используй готовую разметку модального окна с изображением из примеров библиотеки basicLightbox.
+// Разметка элемента галереи
+// Ссылка на оригинальное изображение должна храниться в data - атрибуте source на элементе < img >,
+//     и указываться в href ссылки.Не добавляй другие HTML теги или CSS классы кроме тех, что есть в этом шаблоне.
+
+// <div class="gallery__item">
+//   <a class="gallery__link" href="large-image.jpg">
+//     <img
+//       class="gallery__image"
+//       src="small-image.jpg"
+//       data-source="large-image.jpg"
+//       alt="Image description"
+//     />
+//   </a>
+// </div>
+//     Обрати внимание на то, что изображение обернуто в ссылку, а значит при клике по умолчанию пользователь будет перенаправлен на другую страницу.
+//  Запрети это поведение по умолчанию.
+
+// Закрытие с клавиатуры
+// ВНИМАНИЕ
+// Этот функционал не обязателен при сдаче задания, но будет хорошей дополнительной практикой.
+
+// Добавь закрытие модального окна по нажатию клавиши Escape.Сделай так, чтобы прослушивание клавиатуры было только пока открыто модальное окно.
+//  У библиотеки basicLightbox есть метод для программного закрытия модального окна.
+
+import { galleryItems } from "./gallery-items.js";
+// Change code below this line
+
+const galleryEl = document.querySelector(".gallery");
+const galleryMarkup = createGalleryMarkup(galleryItems);
+
+galleryEl.innerHTML = galleryMarkup;
+
+function createGalleryMarkup(items) {
+  return items
+    .map(
+      ({ preview, original, description }) =>
+        `<div class="gallery__item">
+    <a class="gallery__link" href="${original}">
+      <img
+        class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"
+      />
+    </a>
+  </div>`
+    )
+    .join("");
+}
+
+galleryEl.addEventListener("click", onGalleryItemClick);
+
+function onGalleryItemClick(e) {
+  e.preventDefault();
+
+  if (e.target.nodeName !== "IMG") return;
+
+  const originalUrl = getOriginalUrl(e.target);
+
+  openOriginalImg(originalUrl);
+}
+
+function getOriginalUrl(e) {
+  return e.dataset.source;
+}
+
+function openOriginalImg(originalUrl) {
+  const originalImg = basicLightbox.create(`<img src="${originalUrl}">`, {
+    onShow: () => document.addEventListener("keydown", modalCloseWithEsc),
+    onClose: () => document.removeEventListener("keydown", modalCloseWithEsc),
+  });
+  originalImg.show();
+  document.openedModal = originalImg;
+}
+
+function modalCloseWithEsc(e) {
+  if (e.code === "Escape") {
+    document.openedModal.close();
+    delete document.openedModal;
+  }
+}
+console.log(galleryItems);
